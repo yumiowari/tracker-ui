87005e6d3eaa4de9bf9f29d8ac4d041c20f0942f
Yumiowari <owariyumi@gmail.com>
2025-11-23 16:04:12 -0300

    Estrutura básica:
    - Rote padrão, de login e cadastro
    - Autenticação de sessão

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+app.secret_key = os.getenv('SECRET_KEY')
+
+username = None
+password = None
+
+@app.route('/')
+def index():
+    context = {
+        'title': 'Servidor Flask',
+        'version': '0.0.0'
+    }
+
+    # verifica a sessão
+    if 'user' not in session:
+        return redirect(url_for('login'))
+
+    return render_template('index.html', **context)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global username, password
+
+    if request.method == 'POST':
+        user = request.form.get('username')
+        pswd = request.form.get('password')
+
+        if user == username and pswd == password:
+            session['user'] = user # valida a sessão
+
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Credenciais inválidas.')
+        
+    return render_template('login.html')
+    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    global username, password
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
